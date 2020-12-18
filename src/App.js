ff61f0086a95d677f45f0c1ed92b23a851644edf
Jose Sanrindo <49176895+TakashiJose@users.noreply.github.com>
2020-12-18 14:42:52 -0800

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { getElementError } from '@testing-library/react';
 
+//incomplete
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button id="roll">roll</button>
+      <div id="result"></div>
     </div>
+
   );
 }
+
+let dice=0;
+
+let roll = () =>{
+  dice=Math.floor(Math.random(1,6))
+};
+
+document.getElementById("roll").addEventListener("click", () => roll());
+
+document.getElementById("result").innerHTML=dice;
 
 export default App;
